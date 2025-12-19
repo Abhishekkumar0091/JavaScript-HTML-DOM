fdd470329bf92aca415cdc2336a94c5ff59b270f
Abhishek Kumar <166474204+Abhishekkumar0091@users.noreply.github.com>
2025-12-19 17:29:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// how to select query in js
+// there are four way to select query in js
+// 1. By using Id Selecter
+
+// document.getElementById(abcd);
+// console.dir(abcd);
+
+// 2. By using class Selecter
+// let xyz=document.getElementsByClassName("xyz");
+
+// 3. By using querySelector
+
